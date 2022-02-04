e5409447507dc10f17fde02967e0f1f7fed45d50
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-02-04 15:52:29 +0200

    done

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-let http = require('http'); 
+var http = require('http'); 
 
-let server = http.createServer(function (req, res) {   
+var server = http.createServer(function (req, res) {   
    
-    if (req.url == '/home') {
+    if (req.url == '/data') { //check the URL of the current request
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.write(JSON.stringify({ message: "Hello World"}));  
             res.end();  
     }
-    if (req.url == '/users') {
+    if (req.url == '/users') { //check the URL of the current request
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({ users: ["john","james","jones"]}));  
+        res.write(JSON.stringify({ users:[
+            "muneeb","baidah","rship"
+        ]}));  
         res.end();  
-}
+    } 
+    if (req.url == '/projects') { //check the URL of the current request
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ projects:[
+            "api","js","vuejs"
+        ]}));  
+        res.end();  
+    } 
 });
 
 server.listen(5000);
